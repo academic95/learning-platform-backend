a181579daa10da19d26046996db6431e435a7162
Malandii <malandii.yevhenii@gmail.com>
2026-05-13 18:38:42 +0300

    Implement Laravel Sanctum for API authentication and add necessary migrations and models

---
+++ database/migrations/2026_05_13_132817_create_courses_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->unsignedInteger('duration_hours');
+            $table->boolean('is_mandatory')->default(false);
+            $table->unsignedInteger('topics_count')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
